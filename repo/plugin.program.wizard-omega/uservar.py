7adbb87e087dce57246896ab2c1365a9439553c8
kodiwind <trakt4me@gmail.com>
2025-08-02 15:06:07 +0100

    Updated Repository Omega to 1.1.1
    Updated Repository Omega to 1.1.1
    Added plugin.program.wizard-on=mega.3.0.8

---
+++ repo/plugin.program.wizard-omega/uservar.py
@@ -0,0 +1,16 @@
+import xbmcaddon
+
+'''#####-----Build File-----#####'''
+buildfile = 'https://filedn.com/l0jm1ttNAy54e9NylPPsPVk/Omega/text/builds.xml'
+
+'''#####-----Videos File-----#####'''
+videos_url = 'http://CHANGEME'
+
+'''#####-----Notification File-----#####'''
+notify_url  = 'https://filedn.com/l0jm1ttNAy54e9NylPPsPVk/Omega/text/notify/notify.txt'
+
+'''#####-----Changelog Directory-----#####'''
+changelog_dir  = 'http://CHANGEME/'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
