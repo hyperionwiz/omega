f8f5b59a691b40a85c8e6567a36079aa1833487d
kodiwind <trakt4me@gmail.com>
2026-07-07 03:24:27 +0100

    Updated plugin.video.mando-1.7.5
    Updated plugin.video.mando-1.7.5 from  -1.7.4a

--- repo/plugin.video.mando/resources/lib/modules/kodi_utils.py
+++ repo/plugin.video.mando/resources/lib/modules/kodi_utils.py
@@ -460,6 +460,20 @@ def set_property(prop, value):
 def clear_property(prop):
 	return kodi_window().clearProperty(prop)
 
+def sync_scrape_progress_ui(percent=0, results_sd=0, results_720p=0, results_1080p=0, results_4k=0, results_total=0):
+	set_property('mando.scrape.percent', str(int(percent)))
+	set_property('mando.scrape.results_sd', str(results_sd))
+	set_property('mando.scrape.results_720p', str(results_720p))
+	set_property('mando.scrape.results_1080p', str(results_1080p))
+	set_property('mando.scrape.results_4k', str(results_4k))
+	set_property('mando.scrape.results_total', str(results_total))
+
+def clear_scrape_progress_ui():
+	for prop in ('mando.scrape.percent', 'mando.scrape.results_sd', 'mando.scrape.results_720p',
+			'mando.scrape.results_1080p', 'mando.scrape.results_4k', 'mando.scrape.results_total',
+			'mando.scrape.ready'):
+		clear_property(prop)
+
 def clear_all_properties():
 	return kodi_window().clearProperties()
 
